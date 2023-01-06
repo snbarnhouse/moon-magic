a2549b92c7a64e867a8ab2681ae5a556cc8bc834
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2023-01-05 19:57:44 -0500

    adding initial color gradient

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Moon from "./Moon";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Moon Magic</h1>
+        <Moon></Moon>
       </header>
     </div>
   );
---
+++ src/Moon.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default function Moon(){
+    return(
+        <h2>Hello from Moon</h2>
+    )
+}
